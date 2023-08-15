a3b4c96081189c49dd1b5c796b8c611c1b6f34a8
Vipin M S <40431065+vipinachar@users.noreply.github.com>
2023-08-15 20:30:15 +0530

    new

--- main.tf
+++ main.tf
@@ -31,13 +31,15 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  source = "terraform-aws-modules/security-group/aws"
+  name        = "blog_sg"
+  version     = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
+
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
