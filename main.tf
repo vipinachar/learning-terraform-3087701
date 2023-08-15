e07e866e2da27843d34b704b0f6ae39abc2a033e
Vipin M S <40431065+vipinachar@users.noreply.github.com>
2023-08-15 20:43:48 +0530

    changes to instance deploy

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+  subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,7 +46,7 @@ module "blog_sg" {
   name        = "blog_sg"
   version     = "4.13.0"
 
-  vpc_id      = module.vpc.public_subnets[0]
+  vpc_id      = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
