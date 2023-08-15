d79612abfa0edd598097f0b19ec5b136c0c45658
Vipin M S <40431065+vipinachar@users.noreply.github.com>
2023-08-15 22:25:51 +0530

    Fix

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "ami_filter" {
   })
 
   default = {
-  values = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
   owner  = "979382823631"
   }
 }
